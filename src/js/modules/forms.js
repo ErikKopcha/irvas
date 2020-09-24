0297b4257829dbd477fc94f6cf6b5a5050be6ffe
Erik <erickopcha@gmail.com>
2020-09-24 23:32:07 +0300

    forms module

---
+++ src/js/modules/forms.js
@@ -0,0 +1,64 @@
+const forms = () => {
+    const form = document.querySelectorAll('.form'),
+          inputs = document.querySelectorAll('input'),
+          phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+
+    phoneInputs.forEach(el => {
+      el.addEventListener('input', () => {
+        el.value = el.value.replace(/\D/, ''); // ищем все не цифры
+      });
+    });
+          
+    const message = {
+      loading: 'Загрузка...',
+      success: 'Спасибо! Скоро мы с Вами свяжемся',
+      failure: 'Что-то пошло не так...'
+    };
+
+    const postData = async (url, data) => {
+      document.querySelector('.status').innerHTML = message.loading;
+
+      let res = await fetch(url, {
+        method: 'POST',
+        body: data
+      });
+
+      return await res.text();
+    };
+
+    const clearInputs = () => {
+      inputs.forEach(el => {
+        el.value = '';
+      });
+    };
+
+    form.forEach(el => {
+      el.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+
+        el.appendChild(statusMessage);
+
+        const formData = new FormData(el);
+
+        postData('assets/server.php', formData)
+          .then(res => {
+            console.log(res);
+            statusMessage.textContent = message.success;
+          })
+          .catch(() => {
+            statusMessage.textContent = message.failure;
+          })
+          .finally(() => {
+            clearInputs();
+            setTimeout(() => {
+              statusMessage.remove();
+            }, 5000);
+          });
+      });
+    });
+};
+
+export default forms;
